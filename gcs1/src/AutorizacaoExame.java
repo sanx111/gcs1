ea62a7de7ccd239eeb009a934fea215b2ae06e82
Luiza Coimbra Alfonso <luiza.alfonso@edu.pucrs.br>
2026-05-05 23:19:34 -0300

    criacao classe autorizacao exame

---
+++ gcs1/src/AutorizacaoExame.java
@@ -0,0 +1,78 @@
+import java.time.LocalDate;
+
+public class AutorizacaoExame {
+
+    private static int proximoCodigo = 1000;
+
+    private int codigo;
+    private LocalDate dataCadastro;
+    private Medico medico;
+    private Paciente paciente;
+    private Exame exame;
+    private LocalDate dataRealizacao;
+
+    public AutorizacaoExame(Medico medico, Paciente paciente, Exame exame) {
+        this.codigo = proximoCodigo;
+        proximoCodigo++; // incrementa a cada novo objeto
+
+        this.dataCadastro = LocalDate.now();
+        this.medico = medico;
+        this.paciente = paciente;
+        this.exame = exame;
+        this.dataRealizacao = null;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public LocalDate getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public Exame getExame() {
+        return exame;
+    }
+
+    public LocalDate getDataRealizacao() {
+        return dataRealizacao;
+    }
+
+    public void realizarExame(LocalDate data) {
+        if (data.isBefore(dataCadastro)) {
+            throw new IllegalArgumentException("Data não pode ser antes da solicitação.");
+        }
+
+        if (data.isAfter(dataCadastro.plusDays(30))) {
+            throw new IllegalArgumentException("Exame deve ser realizado em até 30 dias.");
+        }
+
+        this.dataRealizacao = data;
+    }
+
+    //Verifica se já foi realizado
+    public boolean isRealizado() {
+        return dataRealizacao != null;
+    }
+
+    //Representação em texto
+    @Override
+    public String toString() {
+        return "AutorizacaoExame{" +
+                "codigo=" + codigo +
+                ", dataCadastro=" + dataCadastro +
+                ", medico=" + medico.getNome() +
+                ", paciente=" + paciente.getNome() +
+                ", exame=" + exame +
+                ", dataRealizacao=" + dataRealizacao +
+                '}';
+    }
+}
