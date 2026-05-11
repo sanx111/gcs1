50c070b5b93c0684dd1fab72c1057e5fa71a8958
Andre Gomes <gomesandre2105@gmail.com>
2026-05-11 00:27:46 -0300

    feat: adiciona metodo listarPorPaciente em CatalogoAutorizacoes

--- gcs1/src/CatalogoAutorizacoes.java
+++ gcs1/src/CatalogoAutorizacoes.java
@@ -16,5 +16,13 @@ public class CatalogoAutorizacoes {
     public List<AutorizacaoExame> getAutorizacoes() {
         return autorizacoes;
     }
-
+    public List<AutorizacaoExame> listarPorPaciente(Paciente paciente) {
+        List<AutorizacaoExame> resultado = new ArrayList<>();
+        for (AutorizacaoExame a : autorizacoes) {
+            if (a.getPaciente().getId() == paciente.getId()) {
+                resultado.add(a);
+            }
+        }
+        return resultado;
+    }
 }
